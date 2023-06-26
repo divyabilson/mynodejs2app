84bcdf7b3179f098980e827ffa1713d2b4a0f2b6
divyabilson <137600081+divyabilson@users.noreply.github.com>
2023-06-26 18:00:30 +0530

    2 Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require ('express')
 const app = express()
 
-app.get ('/', (req, res) => res.send('Hello Node World'))
+app.get ('/', (req, res) => res.send('Hello Nodejs World'))
 app.listen(3000, () => console.log('erver ready'))
